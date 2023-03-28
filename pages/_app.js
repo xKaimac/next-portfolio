9adc9bd3237e4b6ff09f563893866aa8d27bebee
xKaimac <admin@kaimac.com.au>
2023-03-28 16:08:19 +1030

    changed favicon

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,14 @@ import '@/styles/globals.css';
 import { NavBar } from '@/components/NavBar';
 import { Footer } from '@/components/footer';
 
+import Head from 'next/head';
+
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="../public/favicon.ico" />
+      </Head>
       <link
         href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap"
         rel="stylesheet"
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="shortcut icon" href="/favicon.ico" />  
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap"
