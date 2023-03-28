3db11b6a2e706c71d5bfcbe394a3c9f33d712b68
xKaimac <admin@kaimac.com.au>
2023-03-28 17:49:37 +1030

    fixed the footer to the bottom of the page and added rules for smaller screens

--- pages/portfoliogame.js
+++ pages/portfoliogame.js
@@ -14,7 +14,7 @@ export default function PortFoliogame() {
             <h1 className={styles.title}>
                 Use the arrow keys, or WASD, to move. Press the spacebar to interact with objects when prompted. Each building has an object that can be interacted with. This is shown with a glowing outline of the object.
             </h1>
-        </div>
+        
         <iframe className={styles.game} src="https://portfolio-site-gamma-snowy.vercel.app/pyGame.html" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" allowfullscreen></iframe>
         <br/>
         <p>
@@ -24,6 +24,7 @@ While working on this project, I gained a lot of experience in Python programmin
         <br/><br/>
 The game has been a valuable addition to my portfolio, highlighting my programming skills and creativity. I wrote an article about the development process, which explains the technical details and design choices I made. If you&apos;re interested in game development or Python programming, feel free to check it out!
         </p>
+        </div>
     </div>
     </>
   )
