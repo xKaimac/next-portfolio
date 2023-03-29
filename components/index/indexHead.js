e8b99d3117106909a661de0e57bd828f6065e310
xKaimac <admin@kaimac.com.au>
2023-03-30 09:20:01 +1030

    trying to change the image preview when sending a link

--- components/index/indexHead.js
+++ components/index/indexHead.js
@@ -5,6 +5,7 @@ export function IndexHead() {
     <>
       <Head>
         <title key="title">{"//"}Home</title>
+        <meta property="og:image" content="https://res.cloudinary.com/dhfmjugt0/image/upload/v1680130160/Screenshot_from_2023-03-30_09-19-07_d9pxbg.png" />
       </Head>
     </>
   );
