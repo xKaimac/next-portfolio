ce372add4e0ffdffd00ab1e73f3cac367d491232
xKaimac <admin@kaimac.com.au>
2023-04-01 12:48:50 +1030

    oops i got rid of the favicon and font link from _document head in that last commit

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,18 @@ import Document, { Head, Html, Main, NextScript } from 'next/document'
       render() {
         return (
           <Html lang="en">
-            <Head></Head>
+            <Head>
+            <link rel="shortcut icon" href="/images/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+          <link rel="android-chrome-icon-192x192" sizes="180x180" href="/images/android-chrome-icon-192x192.png" />
+          <link rel="android-chrome-icon-512x512" sizes="180x180" href="/images/android-chrome-icon-512x512.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
+          <link
+          href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+            </Head>
             <body>
               <Main />
               <NextScript />
